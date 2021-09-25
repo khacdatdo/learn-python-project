f6c2ec43240847f48d7548158a796876e334d401
khacdatdo <datdk.b19cn170@stu.ptit.edu.vn>
2021-09-26 03:55:25 +0700

    add data to template

--- base/quizz/views.py
+++ base/quizz/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from quizz.models import Category, LanguageProgramming
 
 # Create your views here.
 def index(request):
-    return render(request, 'quizz/common.html')
+    data = {
+        'categories': Category.objects.all(),
+        'languages': LanguageProgramming.objects.all(),
+    }
+    return render(request, 'quizz/common.html', data)
