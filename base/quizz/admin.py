ba54fab9186577ec75432ca99e8dab0f6d1d767f
khacdatdo <datdk.b19cn170@stu.ptit.edu.vn>
2021-09-26 00:46:57 +0700

    create database tables

--- base/quizz/admin.py
+++ base/quizz/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from quizz.models import Category, LanguageProgramming, Question, Choice
 
 # Register your models here.
+admin.site.register([Category, LanguageProgramming, Question, Choice])
--- base/quizz/models.py
+++ base/quizz/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_key = models.CharField(max_length=200)
+    category_title = models.TextField(max_length=1000)
+
+class LanguageProgramming(models.Model):
+    lg_key = models.CharField(max_length=100)
+    lg_title = models.TextField(max_length=100)
+
+class Question(models.Model):
+    question_text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    language = models.ForeignKey(LanguageProgramming, on_delete=models.CASCADE)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.TextField()
