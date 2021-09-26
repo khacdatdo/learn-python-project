94fe76889fdf06469e1a1eafa87dd04d29eba020
khacdatdo <datdk.b19cn170@stu.ptit.edu.vn>
2021-09-26 22:35:02 +0700

    update admin site

--- base/quizz/admin.py
+++ base/quizz/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from quizz.models import Category, LanguageProgramming, Question, Choice
 
+# Classes
+class ChoicesAdmin(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class QuestionsAdmin(admin.ModelAdmin):
+    fieldsets = [('Question', {'fields': ['question_text', 'category', 'language']})]
+    inlines = [ChoicesAdmin]
+
 # Register your models here.
-admin.site.register([Category, LanguageProgramming, Question, Choice])
+admin.site.register(Question, QuestionsAdmin)
+admin.site.register([Category, LanguageProgramming])
