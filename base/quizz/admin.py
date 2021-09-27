bd3eb08a291150f20063fc8ef7b00b43b8f38d73
khacdatdo <datdk.b19cn170@stu.ptit.edu.vn>
2021-09-27 19:32:09 +0700

    update admin panel

--- base/quizz/admin.py
+++ base/quizz/admin.py
@@ -2,14 +2,31 @@ from django.contrib import admin
 from quizz.models import Category, LanguageProgramming, Question, Choice
 
 # Classes
-class ChoicesAdmin(admin.StackedInline):
+class ChoicesInLine(admin.TabularInline):
     model = Choice
-    extra = 4
+    extra = 1
 
 class QuestionsAdmin(admin.ModelAdmin):
     fieldsets = [('Question', {'fields': ['question_text', 'category', 'language']})]
-    inlines = [ChoicesAdmin]
+    inlines = [ChoicesInLine]
+    ordering = ['id']
+    list_display = ['id', 'question_text' , 'category_title', 'language_title', 'choices']
+    list_display_links = ['question_text']
+    list_filter = ['category', 'language']
+    search_fields = ['id', 'question_text']
+
+class CategoriesAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = ['id', 'category_key', 'category_title', 'questions']
+    list_display_links = ['id', 'category_key', 'category_title']
+    
+class LanguagesAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = ['id', 'lg_key', 'lg_title', 'questions']
+    list_display_links = ['id', 'lg_key', 'lg_title']
+    
 
 # Register your models here.
 admin.site.register(Question, QuestionsAdmin)
-admin.site.register([Category, LanguageProgramming])
+admin.site.register(Category, CategoriesAdmin)
+admin.site.register(LanguageProgramming, LanguagesAdmin)
--- base/quizz/models.py
+++ base/quizz/models.py
@@ -4,21 +4,31 @@ from django.db import models
 
 class Category(models.Model):
     def __str__(self):
-        return str(self.id) + " - " + self.category_key
+        return self.category_key.capitalize()
+    def questions(self):
+        return len(self.question_set.all())
 
     category_key = models.CharField(max_length=200)
-    category_title = models.TextField(max_length=1000)
+    category_title = models.CharField(max_length=500)
 
 class LanguageProgramming(models.Model):
     def __str__(self):
-        return str(self.id) + " - " + self.lg_key
+        return self.lg_key.capitalize()
+    def questions(self):
+        return len(self.question_set.all())
 
     lg_key = models.CharField(max_length=100)
-    lg_title = models.TextField(max_length=100)
+    lg_title = models.CharField(max_length=100)
 
 class Question(models.Model):
     def __str__(self):
-        return str(self.id) + " - " + self.question_text
+        return self.question_text
+    def category_title(self):
+        return self.category.category_title
+    def language_title(self):
+        return self.language.lg_title
+    def choices(self):
+        return len(self.choice_set.all())
 
     question_text = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -26,7 +36,8 @@ class Question(models.Model):
 
 class Choice(models.Model):
     def __str__(self):
-        return str(self.id) + " - " + self.choice_text
+        return self.choice_text
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.TextField()
+    is_correct_answer = models.BooleanField(default=False)
