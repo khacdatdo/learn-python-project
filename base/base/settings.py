41424bf94329875f518461749451297e281b49cb
Đỗ Khắc Đạt <datdk.b19cn170@stu.ptit.edu.vn>
2021-11-14 22:28:22 +0700

    update

--- base/base/settings.py
+++ base/base/settings.py
@@ -43,13 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,11 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# CORS
-CORS_ALLOWED_ORIGINS = [
-    # 'http://127.0.0.1:5500',
-]
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
